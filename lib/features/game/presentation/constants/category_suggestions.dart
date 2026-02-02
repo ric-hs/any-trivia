3bece3cd29c021f7e6d862f8528742467316357b
Rich <ricardoalexis.huerta@gmail.com>
2026-02-02 13:50:48 -0600

    update categories and loading messagges

--- lib/features/game/presentation/constants/category_suggestions.dart
+++ lib/features/game/presentation/constants/category_suggestions.dart
@@ -16,20 +16,61 @@ class CategorySuggestions {
     "Marine Biology",
     "Mythology",
     "Classic Literature",
-    "Movie Directors"
+    "Movie Directors",
+    "General Science",
+    "World History",
+    "Geography",
+    "Literature",
+    "Art History",
+    "Mathematics",
+    "Biology",
+    "Chemistry",
+    "Physics",
+    "Music",
+    "Movies",
+    "Sports",
+    "Politics",
+    "Economics",
+    "Astronomy",
+    "Philosophy",
+    "Technology",
+    "Languages",
   ];
 
   static const List<String> specializedEn = [
     "Disney Classics",
     "Game of Thrones",
     "Malcolm in the Middle",
-    "Formula 1 Racing",
-    "Minecraft Lore",
-    "Medical Breakthroughs",
+    "Medicine",
     "Legal History",
     "The Beatles",
+    "Coding Languages",
+    "Formula 1",
+    "Minecraft",
+    "The Lord of the Rings",
+    "90s Hip Hop",
+    "NASA Space Missions",
+    "Renaissance Painting",
+    "Premier League History",
+    "The Witcher Universe",
+    "Mechanical Engineering",
+    "Pokemon",
+    "True Crime",
     "Modern Architecture",
-    "Coding Languages"
+    "Board Games",
+    "Classic Rock Vinyls",
+    "Viking Age History",
+    "Sushi Varieties",
+    "Mario Kart",
+    "The Simpsons",
+    "How I Met Your Mother",
+    "Artificial Intelligence",
+    "World War II Battles",
+    "Jazz Music",
+    "Baking Techniques",
+    "Yoga Styles",
+    "Harry Potter Spells",
+    "Harry Potter",
   ];
 
   static const List<String> quirkyEn = [
@@ -40,7 +81,29 @@ class CategorySuggestions {
     "Useless Animal Facts",
     "Imaginary Languages",
     "Disco Fashion",
-    "Cryptids & Folklore"
+    "Cryptids & Folklore",
+    "Useless Inventions",
+    "The History of the Sandwich",
+    "Conspiracy Theories about Pigeons",
+    "Strange Local Laws",
+    "Florida Man Headlines",
+    "Forgotten 2000s Memes",
+    "Phobias of Famous People",
+    "Fast Food Secret Menus",
+    "Absurd Victorian Etiquette",
+    "Naming Conventions of IKEA Furniture",
+    "Animals that Look Like Celebrities",
+    "Extinct Birds No One Misses",
+    "Vending Machine Oddities",
+    "The Secret Language of Flowers",
+    "Guinness World Records for Food",
+    "Historical Fashion Disasters",
+    "Tricky Questions",
+    "Unusual Sports",
+    "Strange Holidays",
+    "Bizarre World Records",
+    "Odd Traditions",
+    "Weird Inventions",
   ];
 
   static const List<String> generalEs = [
@@ -51,43 +114,107 @@ class CategorySuggestions {
     "Capitales del Mundo",
     "Cocina Gourmet",
     "Exploración Espacial",
-    "Historia de Videojuegos",
+    "Historia de los Videojuegos",
     "Juegos Olímpicos",
     "Rock & Roll",
     "Arte del Renacimiento",
     "Biología Marina",
     "Mitología",
     "Literatura Clásica",
-    "Directores de Cine"
+    "Directores de Cine",
+    "Ciencia General",
+    "Historia Universal",
+    "Geografía",
+    "Literatura",
+    "Historia del Arte",
+    "Matemáticas",
+    "Biología",
+    "Química",
+    "Física",
+    "Música",
+    "Cine",
+    "Deportes",
+    "Política",
+    "Economía",
+    "Astronomía",
+    "Filosofía",
+    "Tecnología",
+    "Idiomas",
   ];
 
   static const List<String> specializedEs = [
     "Clásicos de Disney",
     "Juego de Tronos",
     "Malcolm el de en medio",
-    "Fórmula 1",
-    "Lore de Minecraft",
-    "Avances Médicos",
-    "Historia de las Leyes",
+    "Medicina",
+    "Derecho",
     "The Beatles",
+    "Programación",
+    "Fórmula 1",
+    "Minecraft",
+    "El Señor de los Anillos",
+    "Hip Hop de los 90",
+    "Misiones Espaciales de la NASA",
+    "Pintura Renacentista",
+    "Historia de la Premier League",
+    "El Universo de The Witcher",
+    "Ingeniería Mecánica",
+    "Pokémon",
+    "True Crime",
     "Arquitectura Moderna",
-    "Lenguajes de Programación"
+    "Juegos de Mesa",
+    "Vinilos de Rock Clásico",
+    "Historia de la Era Vikinga",
+    "Variedades de Sushi",
+    "Mario Kart",
+    "Los Simpson",
+    "Cómo conocí a tu madre",
+    "Inteligencia Artificial",
+    "Batallas de la Segunda Guerra Mundial",
+    "Música Jazz",
+    "Técnicas de Repostería",
+    "Estilos de Yoga",
+    "Hechizos de Harry Potter",
+    "Harry Potter",
   ];
 
   static const List<String> quirkyEs = [
     "Higiene Medieval",
     "Historia de la Tostadora",
     "Jerga del Siglo XIX",
-    "Combinaciones Raras de Comida",
-    "Datos Inútiles de Animales",
+    "Combinaciones de Comida Extrañas",
+    "Datos Inútiles sobre Animales",
     "Idiomas Imaginarios",
     "Moda Disco",
-    "Criptidos y Folclore"
+    "Criptidos y Folklore",
+    "Inventos Inútiles",
+    "La Historia del Sándwich",
+    "Teorías de Conspiración sobre Palomas",
+    "Leyes Locales Extrañas",
+    "Memes Olvidados de los 2000",
+    "Fobias de Personas Famosas",
+    "Menús Secretos de Comida Rápida",
+    "Nomenclatura de Muebles de IKEA",
+    "Animales que se parecen a Famosos",
+    "Aves Extintas que Nadie Extraña",
+    "Curiosidades de Máquinas Expendedoras",
+    "El Lenguaje Secreto de las Flores",
+    "Récords Guinness de Comida",
+    "Desastres de la Moda Histórica",
+    "Preguntas Capciosas",
+    "Deportes Inusuales",
+    "Días Festivos Extraños",
+    "Récords Mundiales Bizarros",
+    "Tradiciones Extrañas",
+    "Inventos Raros",
   ];
 
-  static List<String> getSuggestions(String languageCode, {SuggestionType type = SuggestionType.general}) {
+  static List<String> getSuggestions(
+    String languageCode, {
+    SuggestionType type = SuggestionType.general,
+  }) {
     List<String> originalSuggestions;
-    
+
     if (languageCode == 'es') {
       switch (type) {
         case SuggestionType.specialized:
@@ -97,8 +224,8 @@ class CategorySuggestions {
           originalSuggestions = quirkyEs;
           break;
         case SuggestionType.general:
-        default:
           originalSuggestions = generalEs;
+          break;
       }
     } else {
       switch (type) {
@@ -109,8 +236,8 @@ class CategorySuggestions {
           originalSuggestions = quirkyEn;
           break;
         case SuggestionType.general:
-        default:
           originalSuggestions = generalEn;
+          break;
       }
     }
 
--- lib/features/game/presentation/constants/loading_messages.dart
+++ lib/features/game/presentation/constants/loading_messages.dart
@@ -1,24 +1,34 @@
 class LoadingMessages {
   static const List<String> en = [
-    "Summoning the trivia gods...",
-    "Polishing the trophies...",
-    "Consulting the oracle...",
-    "Loading knowledge crystals...",
-    "Sharpening the questions...",
-    "Preparing your challenge...",
-    "Rolling the dice in the cloud...",
-    "Gathering intelligence..."
+    "Reticulating splines and caffeinating the CPU...",
+    "Consulting the digital oracles...",
+    "Downloading the entire internet. Please hold.",
+    "Teaching the AI the difference between a duck and a dishwasher...",
+    "Searching the deep web for things your teacher never told you.",
+    "Looking for questions about 18th-century hats...",
+    "Fact-checking your weirdest obsessions.",
+    "Wait, you want to play a category about *what*?",
+    "Checking if 'Professional Napping' is a real sport...",
+    "Warming up the neurons...",
+    "Dusting off the Encyclopedia Britannica.",
+    "Gathering the facts. Do you have the answers?",
+    "Synthesizing the ultimate challenge.",
   ];
 
   static const List<String> es = [
-    "Invocando a los dioses de la trivia...",
-    "Puliendo los trofeos...",
-    "Consultando al oráculo...",
-    "Cargando cristales de conocimiento...",
-    "Afilando las preguntas...",
-    "Preparando tu desafío...",
-    "Lanzando los dados en la nube...",
-    "Recopilando inteligencia..."
+    "Dando cafeína al CPU...",
+    "Consultando a los oráculos digitales...",
+    "Descargando todo el internet. Un momento, por favor.",
+    "Enseñándole a la IA la diferencia entre un pato y un lavavajillas...",
+    "Buscando en la deep web cosas que tu profesor nunca te contó.",
+    "Buscando preguntas sobre sombreros del siglo XVIII...",
+    "Verificando datos sobre tus obsesiones más raras.",
+    "Espera, ¿quieres jugar una categoría sobre *qué*?",
+    "Comprobando si 'Siesta Profesional' es un deporte real...",
+    "Calentando las neuronas...",
+    "Desempolvando la Enciclopedia Británica.",
+    "Reuniendo los hechos. ¿Tienes las respuestas?",
+    "Sintetizando el desafío definitivo.",
   ];
 
   static List<String> getMessages(String languageCode) {
